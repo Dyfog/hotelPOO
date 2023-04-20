cad6b1b58ee08237a0916efe903579708c162252
Dyfog <mariolaurie2003@gmail.com>
2023-04-20 09:00:09 -0400

    commit 1, avance inicial codigo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,37 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public static void opcionesMenu(){
+        System.out.println("Tiene las sigueientes opciones:");
+        System.out.println("1.-ingresar un cliente a una haabitacion");
+        System.out.println("2.-Hacer checkout de un cliente");
+        System.out.println("3.-Reseat sistema por cambio de temporada");
+        System.out.println("4.-Salir");
+    }
+    public static int[][] ingresarCliente(int[][] estadoHabitaciones){
+        System.out.println("Tiene las siguientes opciones:");
+        System.out.println("1.-Ingresar a un cliente con una habitacion (sin comida)");
+        System.out.println("2.-Ingresar a un cliente con una habitacion con desayuno y cena incluida");
+        System.out.println("3.-Reservar una habitacion");
+        System.out.println("4.-Salir");
+        switch (eleccion){
+            case 1:
+                break;
+            case 2:
+                System.out.println("Ingresar a un cliente con una habitacion con desayuno y cena incluida");
+                break;
+            case 3:
+                System.out.println("Reservar una habitacion");
+                break;
+            case 4:
+                System.out.println("Salir");
+                break;
+            default:
+                System.out.println("Opción no valida");
+                menu();
+        }
     }
 }
