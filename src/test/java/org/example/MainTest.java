cb8c819f31f551c8fffa669f1b4c4d5638f658c2
Zopa2222 <s.medina08@ufromail.cl>
2023-04-20 14:41:57 -0400

    test 1.0
    Test básicos

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,13 +1,22 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    int[][] habitaciones = new int[3][10];
+
+    @BeforeEach
+    void setUp(){
+        habitaciones[1][1]=2;
+        habitaciones[2][1]=2;
+    }
     @Test
     void testCalcularCosto(){
-
+        assertEquals(80000,Main.calcularCosto(1,habitaciones));
     }
     @Test
     void testIngresarClave(){
