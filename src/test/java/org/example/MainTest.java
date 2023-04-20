bd0ba7c62f80c4587d2c69ca0b76a1694f9f30d0
Dyfog <mariolaurie2003@gmail.com>
2023-04-20 10:52:17 -0400

    Update MainTest.java

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -8,23 +8,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     int[][] habitaciones = new int[3][10];
+    String claveTest1;
+    String claveTest2;
+    String claveTest3;
 
     @BeforeEach
     void setUp(){
         habitaciones[1][1]=2;
         habitaciones[2][1]=2;
+
+        claveTest1 = "1";
+        claveTest2 = "a";
+        claveTest3 = "1234";
     }
     @Test
     void testCalcularCosto(){
         assertEquals(80000,Main.calcularCosto(1,habitaciones));
     }
     @Test
-    void testIngresarClave(){
-
+    void testValidarClave(){
+        assertFalse(Main.validarClave(claveTest1));
+        assertFalse(Main.validarClave(claveTest2));
+        assertTrue(Main.validarClave(claveTest3));
     }
     @Test
-    void testValidarClave(){
-
+    void testestadoHotel(){
+        assertTrue();
     }
-
 }
